371bfef7d4f49c2a75c363abff846b631d6d0128
saintrain <saintrain@mail.ru>
2018-08-30 07:59:12 +0300

    project install

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage", methods={"GET"})
+     */
+    public function indexAction(Request $request)
+    {
+
+        return $this->render('default/index.html.twig', [
+
+        ]);
+    }
+}
